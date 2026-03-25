15b6ffe991a4b1ce852b046be2168ecc12753352
a1masssss <almas.issakov.t@gmail.com>
2026-03-25 21:32:19 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import mimetypes
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def save_binary_file(file_name, data):
+    f = open(file_name, "wb")
+    f.write(data)
+    f.close()
+    print(f"File saved to to: {file_name}")
+
+
+def generate():
+    client = genai.Client(
+        api_key=os.getenv("GEMINI_API_KEY"),
+    )
+
+    model = "lyria-3-pro-preview"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""[0:00 - 0:10] Intro: Intensity: 5/10. generate acoustic song 
+"""),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        response_modalities=[
+            "audio",
+        ],
+    )
+
+    file_index = 0
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        if (
+            chunk.parts is None
+        ):
+            continue
+        if chunk.parts[0].inline_data and chunk.parts[0].inline_data.data:
+            file_name = f"acoustic_song_{file_index}"
+            file_index += 1
+            inline_data = chunk.parts[0].inline_data
+            data_buffer = inline_data.data
+            file_extension = mimetypes.guess_extension(inline_data.mime_type)
+            save_binary_file(f"{file_name}{file_extension}", data_buffer)
+        else:
+            print(chunk.text)
+
+if __name__ == "__main__":
+    load_dotenv()
+    generate()
+
+
